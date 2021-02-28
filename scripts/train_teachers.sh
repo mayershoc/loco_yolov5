9fc26a2a53aef99b5fb6b5873963998722e2a0eb
Christopher Mayershofer <christopher.mayershofer@tum.de>
2021-02-28 23:31:11 +0000

    Add common parameter for training the teacher models

--- scripts/train_teachers.sh
+++ scripts/train_teachers.sh
@@ -1,26 +1,29 @@
 #! /bin/sh
+COMMON="--epochs 30 --batch-size 16"
+
+
 # Pretraining 1: Data: LOCO_synth, Pretrained: No
-python train.py --data loco_synth.yaml --weights '' --name 'pre_synth_p0'
+python train.py --data loco_synth.yaml --weights '' --cfg yolov5x.yaml --name 'pre_synth_p0' $COMMON
 # Pretraining 2: Data: LOCO_synth, Pretrained: COCO
-python train.py --data loco_synth.yaml --weights 'yolov5x.pt' --name 'pre_synth_p1'
+python train.py --data loco_synth.yaml --weights 'yolov5x.pt' --name 'pre_synth_p1' $COMMON
 
 # Teacher 1: Data: LOCO_train, Pretrained: No
-python train.py --data loco_natural.yaml --weights '' --name 'teacher_1'
+python train.py --data loco_natural.yaml --weights '' --cfg yolov5x.yaml --name 'teacher_1' $COMMON
 # Teacher 2: Data: LOCO_train, Pretrained: COCO
-python train.py --data loco_natural.yaml --weights 'yolov5x.pt' --name 'teacher_2'
+python train.py --data loco_natural.yaml --weights 'yolov5x.pt' --name 'teacher_2' $COMMON
 # Teacher 3: Data: LOCO_train, Pretrained: LOCO_synth
-python train.py --data loco_natural.yaml --weights 'runs/train/pre_synth_p0/best.pt' --name 'teacher_3'
+python train.py --data loco_natural.yaml --weights 'runs/train/pre_synth_p0/best.pt' --name 'teacher_3' $COMMON
 # Teacher 4: Data: LOCO_train, Pretrained: COCO & LOCO_synth
-python train.py --data loco_natural.yaml --weights 'runs/train/pre_synth_p1/best.pt' --name 'teacher_4'
+python train.py --data loco_natural.yaml --weights 'runs/train/pre_synth_p1/best.pt' --name 'teacher_4' $COMMON
 
 # Teacher 5: Data: LOCO_train + LOCO_synth, Pretrained: No
-python train.py --data loco_all.yaml --weights '' --name 'teacher_5'
+python train.py --data loco_all.yaml --weights '' --cfg yolov5x.yaml --name 'teacher_5' $COMMON
 # Teacher 6: Data: LOCO_train + LOCO_synth, Pretrained: COCO
-python train.py --data loco_all.yaml --weights 'yolov5x.pt' --name 'teacher_6'
+python train.py --data loco_all.yaml --weights 'yolov5x.pt' --name 'teacher_6' $COMMON
 # Teacher 7: Data: LOCO_train + LOCO_synth, Pretrained: LOCO_synth
-python train.py --data loco_all.yaml --weights 'runs/train/pre_synth_p0/best.pt' --name 'teacher_7'
+python train.py --data loco_all.yaml --weights 'runs/train/pre_synth_p0/best.pt' --name 'teacher_7' $COMMON
 # Teacher 8: Data: LOCO_train + LOCO_synth, COCO & Pretrained: LOCO_synth
-python train.py --data loco_all.yaml --weights 'runs/train/pre_synth_p1/best.pt' --name 'teacher_8'
+python train.py --data loco_all.yaml --weights 'runs/train/pre_synth_p1/best.pt' --name 'teacher_8' $COMMON
 
 
 
